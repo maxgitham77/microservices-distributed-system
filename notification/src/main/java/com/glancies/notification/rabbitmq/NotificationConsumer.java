cfa85aefb61d1365e2f1df2da38330f36ad3d441
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2024-09-15 12:42:35 +0200

    Implemented Notification microservice consuming the message from the queue

--- notification/src/main/java/com/glancies/notification/rabbitmq/NotificationConsumer.java
+++ notification/src/main/java/com/glancies/notification/rabbitmq/NotificationConsumer.java
@@ -1,4 +1,23 @@
 package com.glancies.notification.rabbitmq;
 
+import com.glancies.clients.notification.NotificationRequest;
+import com.glancies.notification.service.SendNotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
 public class NotificationConsumer {
+
+    private final SendNotificationService sendNotificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queues.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed {} from queue", notificationRequest);
+        sendNotificationService.sendNotification(notificationRequest);
+    }
+
 }
